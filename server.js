8af414dab991e1563ec03abfcfd240f7ad21f24b
Alex <canappa@wwu.edu>
2025-04-15 14:53:08 -0400

    got rid of old cards apis

--- server.js
+++ server.js
@@ -337,41 +337,4 @@ app.get('/api/documents/search?q=searchTerm', async (req, res, next) => {
     return res.status(200).json({ documents, error });
 });
 
-
-
-// OLD CARDS LAB API ENDPOINTS
-app.post('/api/addcard', async (req, res, next) => {
-    // incoming: userId, color
-    // outgoing: error
-    const { userId, card } = req.body;
-    const newCard = { Card: card, UserId: userId };
-    var error = '';
-    try {
-        //const db = client.db();
-        const result = db.collection('Cards').insertOne(newCard);
-    }
-    catch (e) {
-        error = e.toString();
-    }
-    cardList.push(card);
-    var ret = { error: error };
-    res.status(200).json(ret);
-});
-
-app.post('/api/searchcards', async (req, res, next) => {
-    // incoming: userId, search
-    // outgoing: results[], error
-    var error = '';
-    const { userId, search } = req.body;
-    var _search = search.trim();
-    //const db = client.db();
-    const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*' } }).toArray();
-    var _ret = [];
-    for (var i = 0; i < results.length; i++) {
-        _ret.push(results[i].Card);
-    }
-    var ret = { results: _ret, error: error };
-    res.status(200).json(ret);
-});
-
 app.listen(5000); // start Node + Express server on port 5000
