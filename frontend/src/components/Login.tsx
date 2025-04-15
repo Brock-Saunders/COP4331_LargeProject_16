b96b996f081cce4fa72dd28f5784e0b764f79ad7
Brock <brockrs@live.com>
2025-04-15 15:27:08 -0400

    Chaged api link in login frontend to localhost for temp local testing

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -27,8 +27,10 @@ function Login({ onLogin }: LoginProps) {
     const obj = { login: loginName, password: loginPassword };
     const js = JSON.stringify(obj);
 
+    // https://largeproj.alexcanapp.xyz/api/users/login
+    //change localhost to largeproj when put on the lightsail server
     try {
-      const response = await fetch('https://largeproj.alexcanapp.xyz/api/users/login', {
+      const response = await fetch('http://localhost:5000/api/users/login', {
         method: 'POST',
         body: js,
         headers: {
