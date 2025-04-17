1b13524025adbd1deaaf28ddc85bbef69bb7c37a
Alex <canappa@wwu.edu>
2025-04-17 00:18:31 -0400

    color change for light mode

--- frontend/src/components/EditorFooter.tsx
+++ frontend/src/components/EditorFooter.tsx
@@ -1,21 +1,21 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
 
 interface EditorFooterProps {
   lastSaved: Date | null;
   lastUpdated: string | null;
   wordCount: number;
-  charCount: number; 
+  charCount: number;
 }
 
 
 
 const formatTime = (date: Date): string => {
-  const now = new Date(); 
-  const diffMs = now.getTime() - date.getTime(); 
+  const now = new Date();
+  const diffMs = now.getTime() - date.getTime();
 
-  const minutes = Math.floor(diffMs / (1000 * 60)); 
-  const hours = Math.floor(diffMs / (100 * 60 * 60)); 
-  const days = Math.floor(diffMs / (1000 * 60 * 60 * 24)); 
+  const minutes = Math.floor(diffMs / (1000 * 60));
+  const hours = Math.floor(diffMs / (100 * 60 * 60));
+  const days = Math.floor(diffMs / (1000 * 60 * 60 * 24));
 
   if (minutes < 1) {
     return "Last Saved just now";
@@ -34,24 +34,24 @@ const EditorFooter: React.FC<EditorFooterProps> = ({ lastSaved, lastUpdated, wor
 
   return (
     <Navbar className="sticky top-0 w-full z-[9999]">
-        <NavbarContent className="" justify="start">
-          <span className="px-4">
+      <NavbarContent className="" justify="start">
+        <span className="px-4">
           {displayTime && (
             <span className="italic text-zinc-400">{formatTime(displayTime)}</span>
           )}
-          </span>
-    
-        </NavbarContent>
-        <NavbarContent justify="end">
-            <NavbarItem>
-            <label className="p-4">
-              Word Count: {wordCount}
-            </label>
-            <label className="p-4">
-                Character Count: {charCount}
-            </label>
-            </NavbarItem>
-        </NavbarContent>  
+        </span>
+
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <label className="p-4 text-black dark:text-white">
+            Word Count: {wordCount}
+          </label>
+          <label className="p-4 text-black dark:text-white">
+            Character Count: {charCount}
+          </label>
+        </NavbarItem>
+      </NavbarContent>
     </Navbar>
   );
 }
