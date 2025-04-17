c94037e4be4d3d743a1317d69ac57de6ba9c6b1e
Alex <canappa@wwu.edu>
2025-04-17 00:32:32 -0400

    yet another llight mode fix

--- frontend/src/components/HeaderEditor.tsx
+++ frontend/src/components/HeaderEditor.tsx
@@ -1,62 +1,62 @@
-import React, {useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit';
 
 interface HeaderEditorProps {
   title: string;
   setTitle: (newTitle: string) => void;
-  onEnter: () => void; 
+  onEnter: () => void;
 }
 
 const HeaderEditor: React.FC<HeaderEditorProps> = ({ title, setTitle, onEnter }) => {
-  const headerContainerRef = useRef<HTMLDivElement>(null); 
-  const headerInstanceRef = useRef<Editor | null> (null); 
-  const [editor, setEditor] = useState<Editor | null>(null); 
-    useEffect(() => {
-      if (headerContainerRef.current) {
-        const newEditor = new Editor({
-          element: headerContainerRef.current, 
-          extensions: [StarterKit], 
-          content: title,
-          editorProps: {
-            attributes: {
-              class: "bg-dark-gray-500 p-4 w-full h-full focus:outline-none", 
-            }, 
-            // switch editors on enter
-            handleKeyDown: (view, event) => {
-              if (event.key === 'Enter') {
-                event.preventDefault(); 
-                onEnter();
-                return true; 
-              }  
-              return false;
-            }, 
-          }, 
-          autofocus: true, 
-          // update header when title changes 
-          onUpdate({ editor }) {
-            setTitle(editor.getText()); 
-          }
-        });
-        headerInstanceRef.current = newEditor; 
-        setEditor(newEditor);
-      }
-  
-      return () => {
-        headerInstanceRef.current?.destroy(); 
-      }
-    }, []); 
-    useEffect(() => {
-      if (editor && editor.getHTML() !== title) {
-        editor.commands.setContent(title, false);
-      }
-    }, [title]);
+  const headerContainerRef = useRef<HTMLDivElement>(null);
+  const headerInstanceRef = useRef<Editor | null>(null);
+  const [editor, setEditor] = useState<Editor | null>(null);
+  useEffect(() => {
+    if (headerContainerRef.current) {
+      const newEditor = new Editor({
+        element: headerContainerRef.current,
+        extensions: [StarterKit],
+        content: title,
+        editorProps: {
+          attributes: {
+            class: "bg-dark-gray-500 text-white p-4 w-full h-full focus:outline-none",
+          },
+          // switch editors on enter
+          handleKeyDown: (view, event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault();
+              onEnter();
+              return true;
+            }
+            return false;
+          },
+        },
+        autofocus: true,
+        // update header when title changes 
+        onUpdate({ editor }) {
+          setTitle(editor.getText());
+        }
+      });
+      headerInstanceRef.current = newEditor;
+      setEditor(newEditor);
+    }
 
-    return (
-      <div className="font-bold text-5xl w-full">
-        <div ref={headerContainerRef} className=''></div>
-      </div>
-    )
+    return () => {
+      headerInstanceRef.current?.destroy();
+    }
+  }, []);
+  useEffect(() => {
+    if (editor && editor.getHTML() !== title) {
+      editor.commands.setContent(title, false);
+    }
+  }, [title]);
+
+  return (
+    <div className="font-bold text-5xl w-full">
+      <div ref={headerContainerRef} className=''></div>
+    </div>
+  )
 
 }
 
