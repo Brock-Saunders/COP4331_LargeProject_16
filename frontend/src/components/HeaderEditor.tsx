4a533ad0d3f5af092bdfe4812a3a88ece8ae975d
Alex <canappa@wwu.edu>
2025-04-17 01:38:30 -0400

    further fixing of title changing

--- frontend/src/components/HeaderEditor.tsx
+++ frontend/src/components/HeaderEditor.tsx
@@ -35,7 +35,9 @@ const HeaderEditor: React.FC<HeaderEditorProps> = ({ title, setTitle, onEnter })
         autofocus: true,
         // update header when title changes 
         onUpdate({ editor }) {
-          setTitle(editor.getHTML());
+          const html = editor.getHTML();
+          const text = new DOMParser().parseFromString(html, 'text/html').body.textContent || '';
+          setTitle(text);
         }
       });
       headerInstanceRef.current = newEditor;
